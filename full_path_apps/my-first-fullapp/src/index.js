72fba60397110e625f015e3a8da480ffee2b3a1b
dembr80 <55060589+dembr80@users.noreply.github.com>
2020-02-25 21:56:34 +0100

    update

--- full_path_apps/my-first-fullapp/src/index.js
+++ full_path_apps/my-first-fullapp/src/index.js
@@ -1,53 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../src/index.css';
 
-class Book extends React.Component {
-  
-  handleClick = () => {           // event figyelés itt a nyilas módszerrel: =()=>
-    const testBook = {
-      title: 'The Quantum Thief',
-      author: 'Hannu Rajaniemi',
-    };
-
-    this.props.addBook(testBook);
-  };
-
-  render() {
-    return <button onClick={this.handleClick}>Add new book</button>;
-  }
-}
-
-class BookStore extends React.Component {
-  constructor(props) {
+class Form extends React.Component {
+    constructor(props) {
     super(props);
-    this.state = {
-      books: {
-        book1: {
-          title: 'American Gods',
-          author: 'Neil Gaiman',
-        },
-        book2: {
-          title: 'Extremely Loud and Incredibly Close',
-          author: 'Johnathan Safran Foer',
-        },
-      },
-    };
+    this.state = { username: '' };
+    }
+    
+    mySubmitHandler = (event) => {
+    event.preventDefault();
+    alert("You are submitting " + this.state.username);
+    this.setState({username: '' });                       //submit után az érték kiürítése
   }
-
-  addBook = book => {
-    // 1. Take a copy of state
-    const newBooks = { ...this.state.books };
-    // 2. Add new item to the copy
-    newBooks[`book${Date.now()}`] = book;
-    // 3. Set the new object to state
-    this.setState({
-      books: newBooks,
-    });
-  };
-
-  render() {
-    return <Book addBook={this.addBook} />;
+ 
+    myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
   }
-}
+ 
+                  
+          render() {
+                  return (
+                    <div className="container">  
+                       <div className="form-input">
+                         <h1>Form</h1>
+                         <form onSubmit={this.mySubmitHandler}>
+                            <label>
+                            Name:
+                            <input type='text'
+                                   value={this.state.username}           //kiinduléskor és submit után a username üres=törli a mezőt
+                                   onChange={this.myChangeHandler}/>
+                           </label>
+                           <input type='submit'/>
+                         </form>  
+                       </div> 
+                       <div className="form-output">
+                         <h1>Output</h1>
+                         <p>Name: {this.state.username}</p>
+                       </div>
+                    </div>  
+                    );
+                  }
+                }
 
-ReactDOM.render(<BookStore />, document.getElementById('root'));
+          ReactDOM.render(<Form />, document.getElementById('root'));
+              
+          
